8817cc0817b00e40260906a26d769cafae0924e2
demiglace0505 <demiglace0505@gmail.com>
2022-02-03 13:59:15 +0800

    testing changes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,7 +5,7 @@ import Layout from '../../components/layout'
 import {getAllPostIds, getPostData} from "../../lib/posts";
 import utilStyles from '../../styles/utils.module.css'
 
-export default function Post({ postData }) {
+export default function Post({postData}) {
   return (
       <Layout>
         <Head>
@@ -14,9 +14,9 @@ export default function Post({ postData }) {
         <article>
           <h1 className={utilStyles.headingXl}>{postData.title}</h1>
           <div className={utilStyles.lightText}>
-            <Date dateString={postData.date} />
+            <Date dateString={postData.date}/>
           </div>
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+          <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
         </article>
       </Layout>
   )
@@ -30,7 +30,7 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({params}) {
   const postData = await getPostData(params.id)
   return {
     props: {
